exthmui-aosp/android_packages_apps_Settings

Migrate away from deprecated Truth APIs.
This is a transitional step towards truth 1.0.1, where these APIs have
been completely removed.

Bug: 168765701
Test: m checkbuild
Change-Id: I76f9c37cb699ce6ab8715ffe35d11668ccbceea1

Former-commit-id: 46e85a2fad30d0fd58302f129d867ad0669c8e20

tests/robotests/src/com/android/settings/password/ChooseLockPatternTest.java
@@ -17,6 +17,7 @@
 package com.android.settings.password;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
 
 import static org.robolectric.RuntimeEnvironment.application;
 
@@ -58,16 +59,13 @@ public class ChooseLockPatternTest {
                 .setUserId(123)
                 .build();
 
-        assertThat(intent
+        assertWithMessage("EXTRA_KEY_FORCE_VERIFY").that(intent
                 .getBooleanExtra(ChooseLockSettingsHelper.EXTRA_KEY_FORCE_VERIFY, false))
-                .named("EXTRA_KEY_FORCE_VERIFY")
                 .isFalse();
-        assertThat((LockscreenCredential) intent
+        assertWithMessage("EXTRA_KEY_PASSWORD").that((LockscreenCredential) intent
                 .getParcelableExtra(ChooseLockSettingsHelper.EXTRA_KEY_PASSWORD))
-                .named("EXTRA_KEY_PASSWORD")
                 .isEqualTo(createPattern("1234"));
-        assertThat(intent.getIntExtra(Intent.EXTRA_USER_ID, 0))
-                .named("EXTRA_USER_ID")
+        assertWithMessage("EXTRA_USER_ID").that(intent.getIntExtra(Intent.EXTRA_USER_ID, 0))
                 .isEqualTo(123);
     }
 
@@ -78,13 +76,11 @@ public class ChooseLockPatternTest {
                 .setUserId(123)
                 .build();
 
-        assertThat(intent
+        assertWithMessage("EXTRA_KEY_REQUEST_GK_PW").that(intent
                 .getBooleanExtra(ChooseLockSettingsHelper.EXTRA_KEY_REQUEST_GK_PW_HANDLE, false))
-                .named("EXTRA_KEY_REQUEST_GK_PW")
                 .isTrue();
-        assertThat(intent
-                .getIntExtra(Intent.EXTRA_USER_ID, 0))
-                .named("EXTRA_USER_ID")
+        assertWithMessage("EXTRA_USER_ID")
+                .that(intent.getIntExtra(Intent.EXTRA_USER_ID, 0))
                 .isEqualTo(123);
     }
 
@@ -94,12 +90,12 @@ public class ChooseLockPatternTest {
                 .setProfileToUnify(23, LockscreenCredential.createNone())
                 .build();
 
-        assertThat(intent.getIntExtra(ChooseLockSettingsHelper.EXTRA_KEY_UNIFICATION_PROFILE_ID, 0))
-                .named("EXTRA_KEY_UNIFICATION_PROFILE_ID")
+        assertWithMessage("EXTRA_KEY_UNIFICATION_PROFILE_ID").that(intent
+                .getIntExtra(ChooseLockSettingsHelper.EXTRA_KEY_UNIFICATION_PROFILE_ID, 0))
                 .isEqualTo(23);
-        assertThat((LockscreenCredential) intent.getParcelableExtra(
-                ChooseLockSettingsHelper.EXTRA_KEY_UNIFICATION_PROFILE_CREDENTIAL))
-                .named("EXTRA_KEY_UNIFICATION_PROFILE_CREDENTIAL")
+        assertWithMessage("EXTRA_KEY_UNIFICATION_PROFILE_CREDENTIAL").that(
+                (LockscreenCredential) intent.getParcelableExtra(
+                        ChooseLockSettingsHelper.EXTRA_KEY_UNIFICATION_PROFILE_CREDENTIAL))
                 .isNotNull();
     }
 

dragonGR/packages_apps_Settings

Merged-In: I76f9c37cb699ce6ab8715ffe35d11668ccbceea1
(cherry picked from commit 46e85a2fad30d0fd58302f129d867ad0669c8e20)

tests/robotests/src/com/android/settings/password/ChooseLockPatternTest.java
@@ -17,6 +17,7 @@
 package com.android.settings.password;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
 
 import static org.robolectric.RuntimeEnvironment.application;
 
@@ -58,16 +59,13 @@ public class ChooseLockPatternTest {
                 .setUserId(123)
                 .build();
 
-        assertThat(intent
+        assertWithMessage("EXTRA_KEY_HAS_CHALLENGE").that(intent
                 .getBooleanExtra(ChooseLockSettingsHelper.EXTRA_KEY_HAS_CHALLENGE, true))
-                .named("EXTRA_KEY_HAS_CHALLENGE")
                 .isFalse();
-        assertThat((LockscreenCredential) intent
+        assertWithMessage("EXTRA_KEY_PASSWORD").that((LockscreenCredential) intent
                 .getParcelableExtra(ChooseLockSettingsHelper.EXTRA_KEY_PASSWORD))
-                .named("EXTRA_KEY_PASSWORD")
                 .isEqualTo(createPattern("1234"));
-        assertThat(intent.getIntExtra(Intent.EXTRA_USER_ID, 0))
-                .named("EXTRA_USER_ID")
+        assertWithMessage("EXTRA_USER_ID").that(intent.getIntExtra(Intent.EXTRA_USER_ID, 0))
                 .isEqualTo(123);
     }
 
@@ -78,17 +76,14 @@ public class ChooseLockPatternTest {
                 .setUserId(123)
                 .build();
 
-        assertThat(intent
+        assertWithMessage("EXTRA_KEY_HAS_CHALLENGE").that(intent
                 .getBooleanExtra(ChooseLockSettingsHelper.EXTRA_KEY_HAS_CHALLENGE, false))
-                .named("EXTRA_KEY_HAS_CHALLENGE")
                 .isTrue();
-        assertThat(intent
+        assertWithMessage("EXTRA_KEY_CHALLENGE").that(intent
                 .getLongExtra(ChooseLockSettingsHelper.EXTRA_KEY_CHALLENGE, 0L))
-                .named("EXTRA_KEY_CHALLENGE")
                 .isEqualTo(12345L);
-        assertThat(intent
+        assertWithMessage("EXTRA_USER_ID").that(intent
                 .getIntExtra(Intent.EXTRA_USER_ID, 0))
-                .named("EXTRA_USER_ID")
                 .isEqualTo(123);
     }
 
@@ -98,12 +93,12 @@ public class ChooseLockPatternTest {
                 .setProfileToUnify(23, LockscreenCredential.createNone())
                 .build();
 
-        assertThat(intent.getIntExtra(ChooseLockSettingsHelper.EXTRA_KEY_UNIFICATION_PROFILE_ID, 0))
-                .named("EXTRA_KEY_UNIFICATION_PROFILE_ID")
+        assertWithMessage("EXTRA_KEY_UNIFICATION_PROFILE_ID").that(intent
+                .getIntExtra(ChooseLockSettingsHelper.EXTRA_KEY_UNIFICATION_PROFILE_ID, 0))
                 .isEqualTo(23);
-        assertThat((LockscreenCredential) intent.getParcelableExtra(
-                ChooseLockSettingsHelper.EXTRA_KEY_UNIFICATION_PROFILE_CREDENTIAL))
-                .named("EXTRA_KEY_UNIFICATION_PROFILE_CREDENTIAL")
+        assertWithMessage("EXTRA_KEY_UNIFICATION_PROFILE_CREDENTIAL").that(
+                (LockscreenCredential) intent.getParcelableExtra(
+                        ChooseLockSettingsHelper.EXTRA_KEY_UNIFICATION_PROFILE_CREDENTIAL))
                 .isNotNull();
     }
 